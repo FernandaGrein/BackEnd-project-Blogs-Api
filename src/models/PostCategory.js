2ec13d7f26716afac146b171dbab21212ae2fcea
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-09-26 15:51:47 -0300

    adiciona requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const postCategorySchema = sequelize.define('PostCategory', {
+    postId: { type: DataTypes.INTEGER, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, foreignKey: true },
+  }, 
+  {
+    timestamps: false,
+    tableName: 'posts_categories',
+    underscored: true,
+  });
+  postCategorySchema.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'blogPosts', 
+      through: postCategorySchema,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'category', 
+      through: postCategorySchema,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  }
+  return postCategorySchema
+}
+
